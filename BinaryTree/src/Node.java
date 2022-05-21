5c91fb80a325758695bb110b4940dfaa396b422a
nickringe <nickringe@gmail.com>
2022-05-21 12:02:15 -0400

    First Commit

---
+++ BinaryTree/src/Node.java
@@ -0,0 +1,41 @@
+
+public class Node {
+
+	public int value;
+	public Node right;
+	public Node left;
+	
+	public Node(int value) {
+		super();
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+	
+	
+	
+	
+	
+}
